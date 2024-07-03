08e697d9c970ae387e9c5d3390c176bebbb63492
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2024-07-03 18:26:50 +0530

    Update ⛩Architectures.py

--- pages/⛩Architectures.py
+++ pages/⛩Architectures.py
@@ -77,7 +77,7 @@ from streamlit_folium import st_folium
 # from pathlib import Path
 # from PIL import Image
 # from streamlit_lottie import st_lottie
-# import time
+import time
 
 st.set_page_config(page_title="NIREEKSHAN - Architecture",page_icon="⛩",layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 _, __, logout= st.columns([5,5,1])
