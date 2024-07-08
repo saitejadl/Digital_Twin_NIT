c921a5932ac295a877e4c00f879568e72cfe954e
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2024-07-08 11:05:44 +0530

    Update ⛩Site_Map.py

--- pages/⛩Site_Map.py
+++ pages/⛩Site_Map.py
@@ -22,7 +22,8 @@ if st.session_state.get('password')!=None:
     st.sidebar.info("🔑SIGNED IN")
     __.title(f"Hi {st.session_state['user_name'].title()}")
     st.write("Site Map")
-    if output.get('last_object_clicked')!=None:
+    
+    if st.session_state.get('last_object_clicked')!=None:
         coordinates = [output["last_object_clicked"]['lat'], output["last_object_clicked"]['lng']]
         st.session_state['loc'] = list(loc.keys())[list(loc.values()).index(coordinates)]
         col1, col2, col3= st.columns(3)
