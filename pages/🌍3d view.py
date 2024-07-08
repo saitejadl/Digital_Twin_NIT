13cf81c3aa5361aa3d636b8d89d702fca208742a
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2024-07-08 10:58:21 +0530

    Update 🌍3d view.py

--- pages/🌍3d view.py
+++ pages/🌍3d view.py
@@ -16,7 +16,7 @@ if logout.button("Logout"):
 if st.session_state.get('load')==None:
   st.session_state['load']=True
   time.sleep(2)
-  st.switch_page(rf"pages/⛩Architectures.py")
+  st.switch_page(rf"pages/⛩Site_Map.py")
 if st.session_state.get('password')!=None:
     st.sidebar.info("🔑SIGNED IN")
     if st.session_state.get('last_object_clicked') != None:
